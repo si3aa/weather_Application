225eeb9a146bd1aed2ef9a96dbf7300d2877af50
si3aa <mahmoud22said22@gmail.com>
2024-07-10 16:00:16 +0300

    create factory constructor

--- lib/models/weather_model.dart
+++ lib/models/weather_model.dart
@@ -1,23 +1,32 @@
 // mvc => model view controller
 
 class WeatherModel {
-  String? cityName;
-  String? country;
-  double? tempC;
-  String? weatherStatus;
-  String? weatherIcon;
-  // WeatherModel({
-  //   required this.cityName,
-  //   required this.country,
-  //   required this.tempC,
-  //   required this.weatherStatus,
-  //   required this.weatherIcon,
-  // });
-  WeatherModel.fromJson(dynamic json) {
-    cityName = json['location']['name'];
-    country = json['location']['country'];
-    tempC = json['current']['temp_c'];
-    weatherStatus = json['current']['condition']['text'];
-    weatherIcon = json['current']['condition']['icon'];
-  }
+  String cityName;
+  String country;
+  double tempC;
+  String weatherStatus;
+  String weatherIcon;
+  WeatherModel({
+    required this.cityName,
+    required this.country,
+    required this.tempC,
+    required this.weatherStatus,
+    required this.weatherIcon,
+  });
+  factory WeatherModel.fromJson(dynamic json) => WeatherModel(
+        cityName: json['location']['name'],
+        country: json['location']['country'],
+        tempC: json['current']['temp_c'],
+        weatherStatus: json['current']['condition']['text'],
+        weatherIcon: json['current']['condition']['icon'],
+      );
 }
+
+
+// {
+//     cityName = json['location']['name'];
+//     country = json['location']['country'];
+//     tempC = json['current']['temp_c'];
+//     weatherStatus = json['current']['condition']['text'];
+//     weatherIcon = json['current']['condition']['icon'];
+//   }
