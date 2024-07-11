d31eaaf5af3c38375d59a46ee778634dac0e3afd
si3aa <mahmoud22said22@gmail.com>
2024-07-11 19:45:11 +0300

    create get_weather method

--- lib/cubit/get_weather/get_cubit.dart
+++ lib/cubit/get_weather/get_cubit.dart
@@ -1,7 +1,16 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:weather/cubit/get_weather/get_state.dart';
+import 'package:weather/models/weather.dart';
+import 'package:weather/services/dio.dart';
 
-class WeatherCubit extends Cubit<WeatherState>{
-  WeatherCubit() :super(InitWeather());
-
+class WeatherCubit extends Cubit<WeatherState> {
+  WeatherCubit() : super(InitWeather());
+  WeatherModel2? weather;
+  void getWeather() async {
+    try {
+      weather = await WeatherDio().getHttp();
+    } catch (e) {
+      print(e);
+    }
+  }
 }
